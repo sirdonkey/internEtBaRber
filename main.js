d8a13c5b3e5bfbadc621c61d08639b955d5e2baf
sirdonkey <126649183+sirdonkey@users.noreply.github.com>
2023-09-28 14:17:36 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+moustache = loadImage("moustache.png")
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+
+function draw() {
+image(video, 0, 0, 300, 300);
+fill(255,0,0);
+stroke(255,0,0);
+image(moustache, noseX, noseY, 30, 30);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x -10;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
